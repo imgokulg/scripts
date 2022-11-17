b8938e440100c164b5bc4a573966c6ae1a315ef5
Gokul G <gokulgunak@gmail.com>
2022-11-17 19:23:28 +0530

    js script changes

--- gokulg.js
+++ gokulg.js
@@ -1,19 +1,36 @@
-function geRandomEmail() {
+function getRandomEmail() {
     let chars = 'abcdefghijklmnopqrstuvwxyz';
     let email = '';
 
-    [['.',6,10],['.',4,7],['@',5,8]].forEach( (eleArr) => {
+    [['.', 6, 10], ['.', 4, 7], ['@', 5, 8]].forEach((eleArr) => {
         email += eleArr[0];
         const min = eleArr[1];
         const max = eleArr[2];
-        let size = geRandomNumber(min, max);
-        for(var index=0; index<size; index++){
+        let size = getRandomNumber(min, max);
+        for (var index = 0; index < size; index++) {
             email += chars[Math.floor(Math.random() * chars.length)];
         }
     });
     return email + '.com';
 }
 
-function geRandomNumber(min,max) {
+function getRandomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function downloadJSON(responseJson, fileName) {
+    if (fileName == undefined || fileName == "") {
+        fileName = location.href.split("/").slice(-1)[0];
+    }
+    const elementId = fileName + "_" + new Date().getTime();
+    document.body.innerHTML += "<a id='"+elementId+"' style='display:none'></a>";
+    responseJson = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(responseJson));
+    let dlAnchorElem = document.getElementById(elementId);
+    dlAnchorElem.setAttribute("href", responseJson);
+    dlAnchorElem.setAttribute("download", fileName + ".json");
+    dlAnchorElem.click();
 }
